1cde80d9ba0fabef1d3b3a053efe350dca30ee11
Adam McMurchie <adammcmurchie@Adams-MacBook-Pro.local>
2020-03-30 17:12:44 +0100

    more

--- test.cs
+++ test.cs
@@ -6,7 +6,7 @@ namespace Giraffe
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("Testing");
         }
     }
 }
